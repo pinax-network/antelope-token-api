3a338bfd421473c3e287daee6c1a5c11f7881c90
Etienne Donneger <etienne@pinax.network>
2024-04-19 12:05:13 -0400

    Add `parsePage` tests

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,16 +1,9 @@
 import { expect, test } from "bun:test";
-import { parseBlockId, parseLimit, parseTimestamp } from "./utils.js";
+import { parseBlockId, parseLimit, parsePage, parseTimestamp } from "./utils.js";
 import { config } from "./config.js";
 
 test("parseBlockId", () => {
     expect(parseBlockId("0x123") as string).toBe("123");
-});
-
-test("parseTimestamp", () => {
-    expect(parseTimestamp("1697587100")).toBe(1697587100);
-    expect(parseTimestamp("1697587100000")).toBe(1697587100);
-    expect(parseTimestamp("awdawd")).toBeNaN();
-    expect(parseTimestamp(null)).toBeUndefined();
 });
 
 test("parseLimit", () => {
@@ -18,4 +11,17 @@ test("parseLimit", () => {
     expect(parseLimit("0")).toBe(1);
     expect(parseLimit(10)).toBe(10);
     expect(parseLimit(config.maxLimit + 1)).toBe(config.maxLimit);
+});
+
+test("parsePage", () => {
+    expect(parsePage("1")).toBe(1);
+    expect(parsePage("0")).toBe(1);
+    expect(parsePage(10)).toBe(10);
+});
+
+test("parseTimestamp", () => {
+    expect(parseTimestamp("1697587100")).toBe(1697587100);
+    expect(parseTimestamp("1697587100000")).toBe(1697587100);
+    expect(parseTimestamp("awdawd")).toBeNaN();
+    expect(parseTimestamp(null)).toBeUndefined();
 });

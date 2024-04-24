138466cca4ffaed705c49aa302223666ca92c351
Etienne Donneger <etienne@pinax.network>
2024-04-24 12:45:29 -0400

    Add `/version` endpoint
    Provide API version and commit hash.

--- src/fetch/GET.ts
+++ src/fetch/GET.ts
@@ -8,6 +8,8 @@ import { logger } from "../logger.js";
 import swaggerHtml from "../../swagger/index.html"
 import swaggerFavicon from "../../swagger/favicon.png"
 import transfers from "./transfers.js";
+import { toJSON } from "./utils.js";
+import { APP_VERSION } from "../config.js";
 
 export default async function (req: Request) {
     const { pathname } = new URL(req.url);
@@ -21,6 +23,7 @@ export default async function (req: Request) {
     if (pathname === "/health") return health(req);
     if (pathname === "/metrics") return new Response(await registry.metrics(), { headers: { "Content-Type": registry.contentType } });
     if (pathname === "/openapi") return new Response(openapi, { headers: { "Content-Type": "application/json" } });
+    if (pathname === "/version") return toJSON({ version: APP_VERSION.split('+')[0], commit: APP_VERSION.split('+')[1] });
     
     // Token endpoints
     if (pathname === "/supply") return supply(req);

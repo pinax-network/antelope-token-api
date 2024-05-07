a48fef2f48d11efde9cf432aa72d0ec937076450
Etienne Donneger <etienne@pinax.network>
2024-05-07 13:44:30 -0400

    Throw error if the request page is beyond the total

--- src/fetch/utils.ts
+++ src/fetch/utils.ts
@@ -26,22 +26,28 @@ export function toJSON(data: any, status: number = 200) {
 
 export function addMetadata(response: Query<any>, req_limit?: number, req_page?: number) {
     // TODO: Catch page number greater than total_pages and return error
-    if (typeof (req_limit) !== 'undefined' && typeof (req_page) !== 'undefined')
+    if (typeof (req_limit) !== 'undefined' && typeof (req_page) !== 'undefined') {
+        const total_pages = Math.ceil(response.rows_before_limit_at_least / req_limit);
+
+        if (req_page > total_pages)
+            throw Error("Requested page exceeds total pages")
+
         return {
             data: response.data,
             meta: {
                 statistics: response.statistics,
-                "next_page": (req_page * req_limit >= response.rows_before_limit_at_least) ? req_page : req_page + 1,
-                "previous_page": (req_page <= 1) ? req_page : req_page - 1,
-                "total_pages": Math.ceil(response.rows_before_limit_at_least / req_limit),
-                "total_results": response.rows_before_limit_at_least
+                next_page: (req_page * req_limit >= response.rows_before_limit_at_least) ? req_page : req_page + 1,
+                previous_page: (req_page <= 1) ? req_page : req_page - 1,
+                total_pages,
+                total_results: response.rows_before_limit_at_least
             }
         }
-    else
+    } else {
         return {
             data: response.data,
             meta: {
                 statistics: response.statistics,
             }
         }
+    }
 }

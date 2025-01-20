5194b1c0ea24fe59ae260dc47c32acaf3a02914a
Etienne Donneger <etienne@pinax.network>
2025-01-20 22:10:01 +0000

    Improve Prometheus Metrics (#81)
    - Added `antelope_token_api` prefix
    - More descriptive names and description for all metrics
    - Added configurable large queries counter monitoring

--- src/prometheus.ts
+++ src/prometheus.ts
@@ -1,12 +1,18 @@
 // From https://github.com/pinax-network/substreams-sink-websockets/blob/main/src/prometheus.ts
 import client, { Counter, CounterConfiguration, Gauge, GaugeConfiguration, Histogram, HistogramConfiguration } from 'prom-client';
 import { logger } from "./logger.js";
+import { config } from './config.js';
 
 export const registry = new client.Registry();
+
+function prefixMetric(name: string): string {
+    return `antelope_token_api_${name}`;
+}
 
 // Metrics
 export function registerCounter(name: string, help = "help", labelNames: string[] = [], config?: Partial<CounterConfiguration<string>>) {
     try {
+        name = prefixMetric(name);
         registry.registerMetric(new Counter({ name, help, labelNames, ...config }));
         logger.debug(`Registered new counter metric: ${name}`);
         return registry.getSingleMetric(name) as Counter;
@@ -18,6 +24,7 @@ export function registerCounter(name: string, help = "help", labelNames: string[
 
 export function registerGauge(name: string, help = "help", labelNames: string[] = [], config?: Partial<GaugeConfiguration<string>>) {
     try {
+        name = prefixMetric(name);
         registry.registerMetric(new Gauge({ name, help, labelNames, ...config }));
         logger.debug(`Registered new gauge metric: ${name}`);
         return registry.getSingleMetric(name) as Gauge;
@@ -29,6 +36,7 @@ export function registerGauge(name: string, help = "help", labelNames: string[] 
 
 export function registerHistogram(name: string, help = "help", labelNames: string[] = [], config?: Partial<HistogramConfiguration<string>>) {
     try {
+        name = prefixMetric(name);
         registry.registerMetric(new Histogram({ name, help, labelNames, ...config }));
         logger.debug(`Registered new histogram metric: ${name}`);
         return registry.getSingleMetric(name) as Histogram;
@@ -36,12 +44,6 @@ export function registerHistogram(name: string, help = "help", labelNames: strin
         logger.error("Error registering histogram:", { name, e });
         throw new Error(`${e}`);
     }
-}
-
-export async function getSingleMetric(name: string) {
-    const metric = registry.getSingleMetric(name);
-    const get = await metric?.get();
-    return get?.values[0]?.value;
 }
 
 function createBucket(lowExponentBound: number, highExponentBound: number): number[] {
@@ -63,20 +65,25 @@ function createBucket(lowExponentBound: number, highExponentBound: number): numb
 }
 
 // REST API metrics
-export const request_error = registerCounter('request_error', 'Total Requests errors', ['pathname', 'status']);
-export const request = registerCounter('request', 'Total Requests', ['pathname']);
-export const query = registerCounter('query', 'Clickhouse DB queries made');
-export const bytes_read = registerHistogram('bytes_read', 'Clickhouse DB Statistics bytes read', [],
-    { 
-        buckets: createBucket(3, 9) // 1Kb to 1Gb buckets, each divided by 10
+export const requests_errors = registerCounter('http_requests_errors_total', 'Total Requests errors by path and status', ['pathname', 'status']);
+export const requests = registerCounter('http_requests_total', 'Total Requests by path', ['pathname']);
+export const queries = registerCounter('ch_queries_total', 'Total ClickHouse DB queries made');
+export const large_queries = registerCounter('ch_large_queries', 'Large ClickHouse DB queries (>10M rows or >1GB read)',
+    ['query_id', 'query', 'query_params', 'bytes_read', 'rows_read', 'elapsed_seconds']
+);
+export const bytes_read = registerHistogram('ch_bytes_read', 'ClickHouse DB aggregated query statistics bytes read', [],
+    {
+        // Create buckets based on large queries trigger setting with a range of 10 values for 6 powers of 10.
+        buckets: createBucket(Math.floor(Math.log10(config.maxBytesTrigger)) - 6, Math.floor(Math.log10(config.maxBytesTrigger)))
     }
 );
-export const rows_read = registerHistogram('rows_read', 'Clickhouse DB Statistics rows read', [],
-    { 
-        buckets: createBucket(2, 7) // 100 to 10M, each divided by 10
+export const rows_read = registerHistogram('ch_rows_read', 'ClickHouse DB aggregated query statistics rows read', [],
+    {
+        // Create buckets based on large queries trigger setting with a range of 10 values for 5 powers of 10.
+        buckets: createBucket(Math.floor(Math.log10(config.maxRowsTrigger)) - 5, Math.floor(Math.log10(config.maxRowsTrigger)))
     }
 );
-export const elapsed = registerHistogram('elapsed', 'Clickhouse DB Statistics query elapsed time (seconds)', [],
+export const elapsed_seconds = registerHistogram('ch_elapsed_seconds', 'ClickHouse DB aggregated query statistics query elapsed time (seconds)', [],
     { 
         buckets: createBucket(-4, 1) // 0.1ms to 10s, each divided by 10
     }
